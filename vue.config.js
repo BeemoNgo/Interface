6ca69d79fb9d16a69c181c0e22bc15e7f4a075bd
BeemoNgo <104055130@student.swin.edu.au>
2024-06-03 17:27:34 +1000

    add config for deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
-const { defineConfig } = require('@vue/cli-service');
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/cos30043/s104055130/dist' : '/',
-});
+// const { defineConfig } = require('@vue/cli-service');
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   publicPath: process.env.NODE_ENV === 'production' ? '/cos30043/s104055130/dist' : '/',
+// });
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Interface/'
+    : '/'
+}
